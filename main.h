ae53eb118d8c612db5f263276d3ec7b595e5c156
N <51336368+Nmcleon@users.noreply.github.com>
2023-04-25 16:24:38 +0200

    one by one test

--- main.h
+++ main.h
@@ -4,4 +4,22 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+/**
+ * struct special_formats - Struct to select specifiers
+ *
+ * @c: data type
+ * @f: The function associated
+ */
+typedef struct special_formats
+{
+	char *c;
+	void (*f)(va_list *, int, int *);
+} s_format;
+
+int _putchar(char c);
+int _printf(const char *, ...);
+int check_input(const char *format, va_list *);
+int print_output(const char *format, va_list *);
+int check_for_format(char, va_list *, int, int *count);
+
 #endif
